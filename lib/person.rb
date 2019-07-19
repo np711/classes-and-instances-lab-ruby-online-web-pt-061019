a35d0422c6e9f2cbabfa4c181b44a7c6e0ab1016
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-19 01:20:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,11 @@
 class Person 
   def initialize (first_name, last_name)
-    @first_name = "adele_goldberg"
+    @first_name = "Adele"
+    @last_name = "Goldberg"
     
-    def initialize (first_last)
-   
+    def initialize (first_last, last_name)
+      @first_name ="Alan"
+      @last_name = "Kay"
+   end 
 end 
     
