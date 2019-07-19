ffeaecd3f020ce6d0ed78bd0de26289029056aca
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-19 01:20:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog 
+  fido =Dog.new 
+  Fido 
+    
+  snoopy =Dog.new 
+  snoopy 
+  
+lassie =Dog.new 
+lassie 
+  
+end 
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person 
+  def initialize (first_name, last_name)
+    @first_name = "Adele"
+    @last_name = "Goldberg"
+    
+    def initialize (first_last, last_name)
+      @first_name ="Alan"
+      @last_name = "Kay"
+   end 
+end 
+    
